443ed0290369d75b39c7fecf6eece7c0b46e92e2
bottom_text <yarik.popov2015@yandex.ru>
2022-09-30 20:30:35 +0300

    Added VMTHooker/other

--- BTMemory/BTMemory.h
+++ BTMemory/BTMemory.h
@@ -1,101 +1,249 @@
 #pragma once
 #include <Windows.h>
 #include <vector>
+#include <psapi.h>
 
+#define k_page_writeable (PAGE_READWRITE | PAGE_EXECUTE_READWRITE)
+#define k_page_readable (k_page_writeable|PAGE_READONLY|PAGE_WRITECOPY|PAGE_EXECUTE_READ|PAGE_EXECUTE_WRITECOPY)
+#define k_page_offlimits (PAGE_GUARD|PAGE_NOACCESS)
+
+
+/* TODO:
+*  JMP/Call hooker :irony:
+*/
 namespace BTMemory {
-	class CPatch {
-		BYTE* m_pOriginBytes;
-		BYTE* m_pPatchBytes;
-		size_t m_uPatchSize;
-		LPVOID m_lpDestination;
-		bool m_bIsNop;
-	public:
-		CPatch(void* destination,  size_t patchSize, const void* patchBytes = 0) {
-			m_lpDestination = (void*)destination;
-			m_uPatchSize = patchSize; 
-			if (patchBytes) {
-				m_pPatchBytes = new BYTE[m_uPatchSize];
-				memcpy(m_pPatchBytes, patchBytes, m_uPatchSize);				
+	namespace VMTHooker {
+		class CVMTHook {
+			void* m_pOriginalVMT;
+			void** m_pVMT;
+			int m_iMethodsCount;
+			int m_iHookedMethod;
+			void* m_fnHookFunction;
+			static std::vector<CVMTHook*> pVMTHooks;
+
+		public:
+			int GetMethodsCount() {
+				int methodsCount = 0;
+
+				while (CanReadPointer(m_pVMT[methodsCount])) {
+					methodsCount++;
+				}
+				return methodsCount;
 			}
-			else {
-				m_bIsNop = true; // we don't need to save patch bytes because we just fill with nops
+
+
+			void* ApplyHook() {
+				void* oldMethod = m_pVMT[m_iHookedMethod];
+				DWORD oldProtect;
+				VirtualProtect(m_pVMT, m_iMethodsCount * sizeof(uintptr_t), PAGE_EXECUTE_READWRITE, &oldProtect);
+				m_pVMT[m_iHookedMethod] = m_fnHookFunction;
+				VirtualProtect(m_pVMT, m_iMethodsCount * sizeof(uintptr_t), oldProtect, &oldProtect);
+				return oldMethod;
 			}
-			CopyOriginBytes();
-		}
-		// actually patching
-		inline void ApplyPatch() {
-			if (m_bIsNop) {
-				Nop(m_lpDestination, m_uPatchSize); // fill with nops...
+			void DestroyHook() {
+				DWORD oldProtect;
+				VirtualProtect(m_pVMT, m_iMethodsCount * sizeof(uintptr_t), PAGE_EXECUTE_READWRITE, &oldProtect);
+				//memcpy(m_pVMT, m_pOriginalVMT, sizeof(uintptr_t) * m_iMethodsCount);			
+				m_pVMT[m_iHookedMethod] = m_pOriginalVMT;
+				VirtualProtect(m_pVMT, m_iMethodsCount * sizeof(uintptr_t), oldProtect, &oldProtect);
+				// delete[] m_pOriginalVMT;
 			}
-			else {
-				Patch(m_lpDestination, m_pPatchBytes, m_uPatchSize); // ...or with patch bytes
+		private:
+			CVMTHook(void* pVMT, int iMethodIndex, void* fnHook) {
+				m_pVMT = (void**)pVMT;
+				m_iMethodsCount = GetMethodsCount();
+				m_pOriginalVMT = m_pVMT[iMethodIndex];
+				m_iHookedMethod = iMethodIndex;
+				m_fnHookFunction = fnHook;
+				//memcpy(m_pOriginalVMT, m_pVMT, sizeof(uintptr_t) * m_iMethodsCount); 
+
 			}
+
+			bool CanReadPointer(void* table) {
+
+				if (!table) {
+					return false;
+				}
+				MEMORY_BASIC_INFORMATION mbi;
+				if (VirtualQuery(table, &mbi, sizeof(mbi)) && !(mbi.Protect & k_page_offlimits) && (mbi.Protect & k_page_readable)) {
+					return true;
+				}
+				return false;
+			}
+
+			friend CVMTHook* Hook(void*, int, void*);
+			friend void UnhookAll();
+		};
+		std::vector<CVMTHook*> CVMTHook::pVMTHooks;
+
+		CVMTHook* Hook(void* pVMT, int iMethodIndex, void* fnHook) {
+			CVMTHook* hook = new CVMTHook(pVMT, iMethodIndex, fnHook);
+			CVMTHook::pVMTHooks.push_back(hook);
+			return hook;
 		}
-		// restore original bytes
-		inline void RestorePatch() {
-			Patch(m_lpDestination, m_pOriginBytes, m_uPatchSize);
+		void UnhookAll() {
+			for (auto& hook : CVMTHook::pVMTHooks) {
+				hook->DestroyHook();
+				delete hook;
+			}
+			CVMTHook::pVMTHooks.clear();
 		}
-		// restore original bytes and delete patch
-		inline void DestroyPatch() {
-			RestorePatch();
-			delete[] m_pOriginBytes; // seems to be no memory leak :trolling:
-			delete[] m_pPatchBytes;
-		}
-		static void Patch(void* destination, const void* patchBytes, size_t patchSize) { // how to "smart" encapsulate them??
-			DWORD oldProtect;
-			VirtualProtect(destination, patchSize, PAGE_EXECUTE_READWRITE, &oldProtect);
-			memcpy(destination, patchBytes, patchSize);
-			VirtualProtect(destination, patchSize, oldProtect, &oldProtect);
-		}
-		static void Nop(void* destination, size_t patchSize) {
-			DWORD oldProtect;
-			VirtualProtect(destination, patchSize, PAGE_EXECUTE_READWRITE, &oldProtect);
-			memset(destination, 0x90, patchSize);
-			VirtualProtect(destination, patchSize, oldProtect, &oldProtect);
-		}
-	
-	private:	
-		void CopyOriginBytes() {
-			m_pOriginBytes = new BYTE[m_uPatchSize];
-			memcpy(m_pOriginBytes, m_lpDestination, m_uPatchSize);
-		}
-		
+
+
 	};
-	
 
-	std::vector<CPatch*> pPatches;
 
-	// ApplyPatch() & RestorePatch() | isSimple = true -> immediate apply/no saving bytes, no restore ( including UnpatchAll() )
-	CPatch* Patch(uintptr_t destination, const void* patchBytes, size_t patchSize, bool isSimple = false) {
-		if (isSimple) {
-			CPatch::Patch((void*)destination, patchBytes, patchSize);
-			return nullptr;
+	namespace Patcher {
+		class CPatch {
+			BYTE* m_pOriginBytes;
+			BYTE* m_pPatchBytes;
+			size_t m_uPatchSize;
+			LPVOID m_lpDestination;
+			bool m_bIsNop;
+			static std::vector<CPatch*> pPatches;
+		public:
+			// actually patching
+			inline void ApplyPatch() {
+				if (m_bIsNop) {
+					Nop(m_lpDestination, m_uPatchSize); // fill with nops...
+				}
+				else {
+					Patch(m_lpDestination, m_pPatchBytes, m_uPatchSize); // ...or with patch bytes
+				}
+			}
+			// restore original bytes
+			inline void RestorePatch() {
+				Patch(m_lpDestination, m_pOriginBytes, m_uPatchSize);
+			}
+			// restore original bytes and delete patch
+			inline void DestroyPatch() {
+				RestorePatch();
+				delete[] m_pOriginBytes; // seems to be no memory leak :trolling:
+				delete[] m_pPatchBytes;
+			}
+		private:
+			CPatch(void* destination, size_t patchSize, const void* patchBytes = 0) {
+				m_lpDestination = (void*)destination;
+				m_uPatchSize = patchSize;
+				if (patchBytes) {
+					m_pPatchBytes = new BYTE[m_uPatchSize];
+					memcpy(m_pPatchBytes, patchBytes, m_uPatchSize);
+				}
+				else {
+					m_bIsNop = true; // we don't need to save patch bytes because we just fill with nops
+				}
+				CopyOriginBytes();
+			}
+
+			static void Patch(void* destination, const void* patchBytes, size_t patchSize) { // how to "smart" encapsulate them??
+				DWORD oldProtect;
+				VirtualProtect(destination, patchSize, PAGE_EXECUTE_READWRITE, &oldProtect);
+				memcpy(destination, patchBytes, patchSize);
+				VirtualProtect(destination, patchSize, oldProtect, &oldProtect);
+			}
+			static void Nop(void* destination, size_t patchSize) {
+				DWORD oldProtect;
+				VirtualProtect(destination, patchSize, PAGE_EXECUTE_READWRITE, &oldProtect);
+				memset(destination, 0x90, patchSize);
+				VirtualProtect(destination, patchSize, oldProtect, &oldProtect);
+			}
+			friend CPatch* Patch(uintptr_t, const void*, size_t, bool);
+			friend CPatch* Nop(uintptr_t, size_t, bool);
+			friend void UnpatchAll();
+
+			void CopyOriginBytes() {
+				m_pOriginBytes = new BYTE[m_uPatchSize];
+				memcpy(m_pOriginBytes, m_lpDestination, m_uPatchSize);
+			}
+
+		};
+		std::vector<CPatch*> CPatch::pPatches;
+
+		// ApplyPatch() & RestorePatch() | isSimple = true -> immediate apply/no saving bytes, no restore ( including UnpatchAll() )
+		CPatch* Patch(uintptr_t destination, const void* patchBytes, size_t patchSize, bool isSimple = false) {
+			if (isSimple) {
+				CPatch::Patch((void*)destination, patchBytes, patchSize);
+				return nullptr;
+			}
+			else {
+				CPatch* patch = new CPatch((void*)destination, patchSize, patchBytes);
+				CPatch::pPatches.push_back(patch);
+				return patch;
+			}
 		}
-		else {
-			CPatch* patch = new CPatch((void*)destination, patchSize, patchBytes);
-			pPatches.push_back(patch);
-			return patch;
+
+		// ApplyPatch() & RestorePatch() | isSimple = true -> immediate apply/no saving bytes, no restore ( including UnpatchAll() )
+		CPatch* Nop(uintptr_t destination, size_t patchSize, bool isSimple = false) {
+			if (isSimple) {
+				CPatch::Nop((void*)destination, patchSize);
+				return nullptr;
+			}
+			else {
+				CPatch* patch = new CPatch((void*)destination, patchSize, 0);
+				CPatch::pPatches.push_back(patch);
+				return patch;
+			}
 		}
+		void UnpatchAll() {
+			for (auto& patch : CPatch::pPatches) {
+				patch->DestroyPatch();
+				delete patch;
+			}
+		}
+	};
+
+	uintptr_t FindSignature(const char* moduleName, const void* signatureString, const char* mask) {
+
+		unsigned char* signature = (unsigned char*)signatureString; // something is trolling us
+		HMODULE moduleHandle = GetModuleHandle(moduleName);
+		if (!moduleHandle) {
+			return NULL;
+		}
+
+		uintptr_t moduleBaseAddress = (uintptr_t)moduleHandle;
+		MODULEINFO moduleInfo;
+		GetModuleInformation(GetCurrentProcess(), moduleHandle, &moduleInfo, sizeof(MODULEINFO));
+		uintptr_t moduleEndAddress = moduleBaseAddress + moduleInfo.SizeOfImage;
+
+		size_t signatureLength = strlen(mask);
+
+		for (uintptr_t address = moduleBaseAddress; address < moduleEndAddress - signatureLength; address++) {
+			for (unsigned int signatureIterator = 0; signatureIterator < signatureLength; signatureIterator++) {
+				if (mask[signatureIterator] == 'x') {
+					if (*reinterpret_cast<BYTE*>(address + signatureIterator) != signature[signatureIterator]) {
+						break;
+					}
+					else {
+						if (signatureIterator == signatureLength - 1) { // parsed all signature;
+							return address;
+						}
+					}
+				}
+			}
+
+		}
+		return 0;
 	}
-	// ApplyPatch() & RestorePatch() | isSimple = true -> immediate apply/no saving bytes, no restore ( including UnpatchAll() )
-	CPatch* Nop(uintptr_t destination, size_t patchSize, bool isSimple = false) {
-		if (isSimple) {
-			CPatch::Nop((void*)destination, patchSize);
-			return nullptr;
+
+
+	uintptr_t FindDMAAddy(uintptr_t baseAddress, std::vector<unsigned int> offsets) {
+		for (auto offset : offsets) {
+			baseAddress = *reinterpret_cast<uintptr_t*>(baseAddress);
+			baseAddress += offset;
 		}
-		else {
-			CPatch* patch = new CPatch((void*)destination, patchSize, 0);
-			pPatches.push_back(patch);
-			return patch;
-		}
+		return baseAddress;
 	}
 
+	/*void* GetVMTMethod(void* pObject, int iMethodIndex) {
+		return (*(void***)pObject)[iMethodIndex];
+	}
+	*/
+
+	// undone all hooks (VMT / JMP etc.)/patches
 	void UnpatchAll() {
-		for (auto& patch : pPatches) {
-			patch->DestroyPatch();
-			delete patch;
-		}
-		pPatches.clear();
+		Patcher::UnpatchAll();
+		VMTHooker::UnhookAll();
 	}
+
 }
 
--- BTMemory/main.cpp
+++ BTMemory/main.cpp
@@ -5,7 +5,7 @@
 DWORD WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved) {
 	switch (dwReason) {
 	case DLL_PROCESS_ATTACH:
-		static BTMemory::CPatch* noRecoilNop = BTMemory::Nop((DWORD)GetModuleHandle(NULL) + 0x63786, 10); // nop the function call
+		static auto* noRecoilNop = BTMemory::Patcher::Nop((DWORD)GetModuleHandle(NULL) + 0x63786, 10); // nop the function call
 		// or
 		//static BTMemory::CPatch* noRecoilPatch = BTMemory::Patch((DWORD)GetModuleHandle(NULL) + 0x62020, "\xC2\x80\x00", 3); // "ret 08" to prevent execution of function
 		noRecoilNop->ApplyPatch();
@@ -22,13 +22,13 @@ DWORD WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved) {
 	Other examples: 
 
 
-	BTMemory::Nop(0x228, 3, true); // the last parameter is true -> return value is nullptr, so immediate apply and no restore possibility
-	BTMemory::Patch(0x228, "\0xE9\x13\x37"3, true);
+	BTMemory::Patcher::Nop(0x228, 3, true); // the last parameter is true -> return value is nullptr, so immediate apply and no restore possibility
+	BTMemory::Patcher::Patch(0x228, "\0xE9\x13\x37"3, true);
 
 	/////////////////////////////////////////////////////////
 
 	settings::bNoRecoil = !settings::bNoRecoil;
-	static BTMemory::CPatch* noRecoilPatch = BTMemory::Nop(globals::baseAddress + 0x63786, 10);
+	static auto* noRecoilPatch = BTMemory::PatcherNop(globals::baseAddress + 0x63786, 10);
 	if (settings::bNoRecoil) {
 		noRecoilPatch->ApplyPatch(); // change the bytes to ours
 	}
@@ -37,5 +37,31 @@ DWORD WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved) {
 	}
 
 	printf("No recoil: %s\n", settings::bNoRecoil ? "on" : "off");
+
+	/////////////////////////////////////////////////////////
+
+	typedef void(__thiscall* recoil_t)(DWORD*, float*, float*);
+	recoil_t oRecoil;
+
+	void __fastcall recoil(DWORD* pThis, void* edx, float* a2, float* a3) {
+		printf("%0.2f | %0.2f\n", *a2, *a3);
+		return oRecoil(pThis, a2, a3);
+	}
+
+
+	void* pVMT = *(void**)Misc->GetLocalPlayer()->GetWeaponById(Weapons::SUBGUN);
+
+	auto* recoilHook = BTMemory::VMTHooker::Hook(pVMT, 5, &recoil);
+	oRecoil = (recoil_t)recoilHook->ApplyHook();
+
+	//////////////////////////////////////////////////////////
+
+	uintptr_t sigResult = BTMemory::FindSignature("ac_client.exe", "\x8B\x46\x0C\x0F\xBF\x88\x00\x00\x00\x00\x8B\x56\x18\x89\x0A\x8B\x76\x14\xFF\x0E",
+					"xxxxxx????xxxxxxxxxx");
+	if (sigResult) {
+		sigResult += 0x12;
+		BTMemory::Patcher::Nop(sigResult, 2, true);
+	}
+
 
 */
